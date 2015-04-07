bcfd9dbc5d5df905a43f617b579177f74e280cc0
Mann <chanmannlim@gmail.com>
2015-04-06 21:13:28 -0500

    pd_prime

--- BN1.m
+++ BN1.m
@@ -9,20 +9,21 @@ display('                                                       ');
 display('                 Bayesian Networks No.1                ');
 display('_______________________________________________________');
 display(' ');
+
 %% Run EM algorithm and learn CPDs
 domain = ['H' 'M' 'L'];
 P_Pd = Pr(Pd_train, [1 0]');
 P_Pd_prime_given_Pd = CPT(Pd_prime_train, Pd_train);
-P_Xb_given_Pd_prime = EM(Xb_train, Pd_train, [theta_0(domain); theta_0(domain)]);
-P_Xh_given_Pd_prime = EM(Xh_train, Pd_train, [theta_0(domain); theta_0(domain)]);
-P_Xt_given_Pd_prime = EM(Xt_train, Pd_train, [theta_0(domain); theta_0(domain)]);
+P_Xb_prime_given_Pd_prime = EM(Xb_prime_train, Pd_prime_train, [theta_0(domain); theta_0(domain)]);
+P_Xh_prime_given_Pd_prime = EM(Xh_prime_train, Pd_prime_train, [theta_0(domain); theta_0(domain)]);
+P_Xt_prime_given_Pd_prime = EM(Xt_prime_train, Pd_prime_train, [theta_0(domain); theta_0(domain)]);
 
 display('--------------------- Parameters learned ---------------------');
 display(P_Pd);
 display(P_Pd_prime_given_Pd);
-display(P_Xb_given_Pd_prime);
-display(P_Xh_given_Pd_prime);
-display(P_Xt_given_Pd_prime);
+display(P_Xb_prime_given_Pd_prime);
+display(P_Xh_prime_given_Pd_prime);
+display(P_Xt_prime_given_Pd_prime);
 
 %% Confusion matrix (Train)
 observation = [Xb_train Xh_train Xt_train];
