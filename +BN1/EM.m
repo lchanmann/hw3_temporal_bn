1b74a05f9fea206c85629b4c5f8dd7ffaaf0084e
Fernando Torre Mora <torrefernand@gmail.com>
2015-04-07 22:53:04 -0500

    EM for networ 2
    now in the right place!

--- +BN1/EM.m
+++ +BN1/EM.m
@@ -1,11 +1,9 @@
-function [ theta ] = EM( x, y, Pd, cpt )
-% EM - Expectation Maximization algorithm for a variable depending on
-%      two other variables.
-% inputs
+function [ theta ] = EM( x, Pd, cpt )
+% EM - Expectation Maximization algorithm
 
     epsilon = 10e-5;
     while 1
-        theta = em_step(x, y, Pd, cpt);
+        theta = em_step(x, Pd, cpt);
         distance = norm(theta - cpt);
         if distance < epsilon
             break
@@ -15,21 +13,18 @@ function [ theta ] = EM( x, y, Pd, cpt )
 end
 
 %% em_step - one iteration of em
-function [theta] = em_step( x, y, Pd, cpt )
- em
+function [theta] = em_step( x, Pd, cpt )
     pd_domain = [1 0];
     domain = ['H' 'M' 'L'];
     
     [row, column] = size(cpt);
-    theta = zeros(row, column, page);
-    for m=1:page
-        for k=1:row
-            pd_is_k = (Pd == pd_domain(k));
-            pd_count = sum(pd_is_k);
-            for l=1:column
-                expected_sum = sum(x == domain(l) & pd_is_k) + cpt(k, l) * sum(isnan(x) & pd_is_k);
-                theta(k, l) = expected_sum / pd_count;
-            end
+    theta = zeros(row, column);
+    for k=1:row
+        pd_is_k = (Pd == pd_domain(k));
+        pd_count = sum(pd_is_k);
+        for l=1:column
+            expected_sum = sum(x == domain(l) & pd_is_k) + cpt(k, l) * sum(isnan(x) & pd_is_k);
+            theta(k, l) = expected_sum / pd_count;
         end
     end
 end
--- +BN2/EM.m
+++ +BN2/EM.m
@@ -1,9 +1,11 @@
-function [ theta ] = EM( x, Pd, cpt )
-% EM - Expectation Maximization algorithm
+function [ theta ] = EM( x, y, Pd, cpt )
+% EM - Expectation Maximization algorithm for a variable depending on
+%      two other variables.
+% inputs
 
     epsilon = 10e-5;
     while 1
-        theta = em_step(x, Pd, cpt);
+        theta = em_step(x, y, Pd, cpt);
         distance = norm(theta - cpt);
         if distance < epsilon
             break
@@ -13,18 +15,23 @@ function [ theta ] = EM( x, Pd, cpt )
 end
 
 %% em_step - one iteration of em
-function [theta] = em_step( x, Pd, cpt )
-    pd_domain = [1 0];
+function [theta] = em_step( x, y, Pd, cpt )
+ em
+    pd_domain = [1 2 3];
     domain = ['H' 'M' 'L'];
     
-    [row, column] = size(cpt);
-    theta = zeros(row, column);
-    for k=1:row
+    [row, column, page] = size(cpt);
+    theta = zeros(row, column, page);
+    for k=1:page
         pd_is_k = (Pd == pd_domain(k));
         pd_count = sum(pd_is_k);
-        for l=1:column
-            expected_sum = sum(x == domain(l) & pd_is_k) + cpt(k, l) * sum(isnan(x) & pd_is_k);
-            theta(k, l) = expected_sum / pd_count;
+        for l=1:row
+            x_count = sum(pd_is_x);
+            for m=1:column
+                expected_sum = sum(x == domain(l) & y == domain(m) & pd_is_k) ...
+                    + cpt(l, m, k) * sum(isnan(y) & pd_is_k);
+                theta(l, m, k) = expected_sum / pd_count;
+            end
         end
     end
 end
