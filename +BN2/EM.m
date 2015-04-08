462755504ad5ace687557707c01104e6200ca93f
Fernando Torre Mora <torrefernand@gmail.com>
2015-04-08 00:50:57 -0500

    EM for network 2
    Now 100% more generalizable and complete

--- +BN2/EM.m
+++ +BN2/EM.m
@@ -1,11 +1,20 @@
-function [ theta ] = EM( x, y, Pd, cpt )
+function [ theta ] = EM( x, y, Pd, cpt, x_domain, y_domain, pd_domain  )
 % EM - Expectation Maximization algorithm for a variable depending on
 %      two other variables.
 % inputs
+    if nargin<7 %assume Xa
+        pd_domain = [1 2 3];
+    end
+    if nargin<6 %assume Xb/Xh/Xt
+        y_domain = ['H' 'M' 'L'];
+    end
+    if nargin<5 %assume Xb/Xh/Xt
+        x_domain = ['H' 'M' 'L'];
+    end
 
     epsilon = 10e-5;
     while 1
-        theta = em_step(x, y, Pd, cpt);
+        theta = em_step(x, y, Pd, cpt, x_domain, y_domain, pd_domain);
         distance = norm(theta - cpt);
         if distance < epsilon
             break
@@ -15,21 +24,23 @@ function [ theta ] = EM( x, y, Pd, cpt )
 end
 
 %% em_step - one iteration of em
-function [theta] = em_step( x, y, Pd, cpt )
- em
-    pd_domain = [1 2 3];
-    domain = ['H' 'M' 'L'];
-    
+function [theta] = em_step( x, y, Pd, cpt, x_domain, y_domain, pd_domain )
     [row, column, page] = size(cpt);
     theta = zeros(row, column, page);
     for k=1:page
         pd_is_k = (Pd == pd_domain(k));
+        pd_is_nan = isnan(Pd);
         pd_count = sum(pd_is_k);
         for l=1:row
-            x_count = sum(pd_is_x);
             for m=1:column
-                expected_sum = sum(x == domain(l) & y == domain(m) & pd_is_k) ...
-                    + cpt(l, m, k) * sum(isnan(y) & pd_is_k);
+                missingValues = sum(x == x_domain(l) & isnan(y) & pd_is_k)...
+                    + sum(isnan(x) & y == y_domain(m) & pd_is_k)...
+                    + sum(isnan(x) & isnan(y) & pd_is_k)...
+                    + sum(x == x_domain(l) & isnan(y) & pd_is_nan)...
+                    + sum(isnan(x) & y == y_domain(m) & pd_is_nan)...
+                    + sum(isnan(x) & isnan(y) & pd_is_nan)...
+                    +sum(x == x_domain(l) & y == y_domain(m) & pd_is_nan);
+                expected_sum = sum(x == x_domain(l) & y == y_domain(m) & pd_is_k) + cpt(l, m, k) * missingValues;
                 theta(l, m, k) = expected_sum / pd_count;
             end
         end
