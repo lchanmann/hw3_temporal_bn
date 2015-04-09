7e02383f23232983a393ecd94709f4ec964f4c3a
Mann <chanmannlim@gmail.com>
2015-04-08 22:12:27 -0500

    bn2 cpt learning with laplace smoothing

--- +BN2/CPT.m
+++ +BN2/CPT.m
@@ -1,44 +1,44 @@
-function [ cpt ] = CPT ( Pd_prime , Pd, Xa )
+function [ cpt ] = CPT ( x , Pd, Xa, domain )
 % BN2.CPT - construct CPT for a random variable in BN 2
 %   learn CPT parameters using Maximum Likelihood estimator
 %
 % Input:
-%   Pd - discrete random variable that Pd_prime depends on. Domain = {0, 1}
-%   Pd_prime - evidence variable 'drink'. Domain = {0, 1}
-%   Xa - conditional variable 'Ambulation status'. Domain = {1 2 3}
+%   x - discrete random variable. Domain = {H, M, L}
+%   Pd - evidence variable 'drink'. Domain = {1 0}
+%   Xa - evidence variable 'Ambulation status'. Domain = {1 2 3}
 %
 % Output:
 %   cpt - conditional probability table of the random variable given the
 %         evidence drink and ambulation status.
 %        eg.
-%           
-%                               Pd
-%              |Stationary |   Slow    |   Fast    |
-%              |  0  |  1  |  0  |  1  |  0  |  1  |
-% Pd_prime = 0 |     |     |     |     |     |     |
-% Pd_prime = 1 |     |     |     |     |     |     |
-% 
+%            If domain = ['H', 'M', 'L']
+%
+%                           Xb
+%           |Stationary |   Slow    |   Fast    |
+%        Pd | H | M | L | H | M | L | H | M | L |
+%         1 |   |   |   |   |   |   |   |   |   | 
+%         0 |   |   |   |   |   |   |   |   |   |
 % Note:
 %   The elements/rows of evidences must correspond to x's
 %
-    domain = [0 1];
+
     xa_domain = [1 2 3];
     cpt = zeros(2, length(domain) * length(xa_domain));
+    laplace_k = get_laplace_k;
+    classes = length(domain);
     
-    %               count(Pd_prime = {0,1} , pd = {0, 1}, xa = {1 2 3})
+    %               count(x = {H, M, L} , pd = {1, 0}, xa = {1 2 3})
     % likelihood = --------------------------------------------------
-    %                      count(pd = {0, 1}, xa = {1 2 3})
+    %                      count(pd = {1, 0}, xa = {1 2 3})
     for k = 1:length(xa_domain)
-        Pd_is_0_and_xa = sum(Pd == 0 & Xa == xa_domain(k));
         Pd_is_1_and_xa = sum(Pd == 1 & Xa == xa_domain(k));
+        Pd_is_0_and_xa = sum(Pd == 0 & Xa == xa_domain(k));
         
         for i = 1:length(domain)
-            x_i = Pd(Pd_prime == domain(i) & Xa == xa_domain(k));
+            x_i = Pd(x == domain(i) & Xa == xa_domain(k));
             col = i + (k-1) * length(domain);
-            cpt(1, col) = sum(x_i == 0) / Pd_is_0_and_xa;
-            cpt(2, col) = sum(x_i == 1) / Pd_is_1_and_xa;
+            cpt(1, col) = (sum(x_i == 1) + laplace_k) / (Pd_is_1_and_xa + laplace_k * classes);
+            cpt(2, col) = (sum(x_i == 0) + laplace_k) / (Pd_is_0_and_xa + laplace_k * classes);
         end
     end
-    % normalize lack of data samples
-    cpt(isnan(cpt)) = 0;
 end
--- BN2.m
+++ BN2.m
@@ -15,17 +15,30 @@ no_missing_Xa = fill_best( Xa_train );
 no_missing_Xb = fill_best_xi( Xb_train, no_missing_Pd, no_missing_Xa );
 no_missing_Xh = fill_best_xi( Xh_train, no_missing_Pd, no_missing_Xa );
 no_missing_Xt = fill_best_xi( Xt_train, no_missing_Pd, no_missing_Xa );
+no_missing_Pd_prime = no_missing_Pd(2:end);
+no_missing_Xa_prime = no_missing_Xa(2:end);
+no_missing_Pd = no_missing_Pd(1:end-1);
+no_missing_Xa = no_missing_Xa(1:end-1);
+no_missing_Xb = no_missing_Xb(1:end-1);
+no_missing_Xh = no_missing_Xh(1:end-1);
+no_missing_Xt = no_missing_Xt(1:end-1);
 
 P_Pd = Pr(no_missing_Pd, [1 0]');
 P_Xa = Pr(no_missing_Xa, [1 2 3]');
-P_Xa_given_Pd_and_Xa = BN2.CPT(no_missing_Xb, no_missing_Pd, no_missing_Xa);
+P_Xa_given_Pd_and_Xa = BN2.CPT(no_missing_Xb, no_missing_Pd, no_missing_Xa, 'HML');
+P_Xh_given_Pd_and_Xa = BN2.CPT(no_missing_Xh, no_missing_Pd, no_missing_Xa, 'HML');
+P_Xt_given_Pd_and_Xa = BN2.CPT(no_missing_Xt, no_missing_Pd, no_missing_Xa, 'HML');
+P_Pd_prime_given_Pd_and_Xa = BN1.CPT(no_missing_Pd_prime, no_missing_Pd);
+P_Xa_prime_given_Pd_and_Xa = BN2.CPT(no_missing_Xa_prime, no_missing_Pd, no_missing_Xa, 1:3);
 
-% P_Pd_prime = BN2.CPT(Pd_train(1:length(Pd_train)-1), Pd_train(2:length(Pd_train)), Xa_prime_train);
-P_Pd_prime_given_Pd = BN1.CPT(Pd_prime_train, Pd_train);
-% P_Xa_prime_given_Xa_and_Pd = BN2.CPT(Xa_prime_train, Pd_train, Xa_train);
-% P_Xb_prime_given_Pd_prime = EM(Xb_prime_train, Pd_prime_train, initialize_cpt());
-% P_Xh_prime_given_Pd_prime = EM(Xh_prime_train, Pd_prime_train, initialize_cpt());
-% P_Xt_prime_given_Pd_prime = EM(Xt_prime_train, Pd_prime_train, initialize_cpt());
+display('--------------------- Parameters learned ---------------------');
+display(P_Pd);
+display(P_Xa);
+display(P_Xa_given_Pd_and_Xa);
+display(P_Xh_given_Pd_and_Xa);
+display(P_Xt_given_Pd_and_Xa);
+display(P_Pd_prime_given_Pd_and_Xa);
+display(P_Xa_prime_given_Pd_and_Xa);
 
 %% Confusion matrix (Train)
 observation = [Pd_train Xa_train Xb_prime_train Xa_prime_train Xh_prime_train Xt_prime_train];
