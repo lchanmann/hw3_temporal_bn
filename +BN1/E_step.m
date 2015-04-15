0a8dad42cc9a28c950dc04a6956a366d894aa1b0
Mann <chanmannlim@gmail.com>
2015-04-14 20:29:41 -0500

    rename

--- +BN1/E_step.m
+++ +BN1/E_step.m
@@ -2,23 +2,19 @@
 model = BN1.model( P_Pd, P_Pd_prime_given_Pd, ...
     P_Xb_prime_given_Pd_prime, P_Xh_prime_given_Pd_prime, P_Xt_prime_given_Pd_prime);
 
-Pd = [0 0];
-Pd_prime_given_Pd = zeros(2,2);
-Xb_given_Pd = zeros(2, 3);
-Xh_given_Pd = zeros(2, 3);
-Xt_given_Pd = zeros(2, 3);
+Pd_count = [0 0];
+Pd_prime_given_Pd_count = zeros(2,2);
+Xb_prime_given_Pd_prime_count = zeros(2, 3);
+Xh_prime_given_Pd_prime_count = zeros(2, 3);
+Xt_prime_given_Pd_prime_count = zeros(2, 3);
 
 for i=1:row
-    sample = [Xb_train(i) Xh_train(i) Xt_train(i) Pd_train(i) ...
-        Pd_prime_train(i) Xb_prime_train(i) Xh_prime_train(i) Xt_prime_train(i)];
-    
-    Pd = Pd + model.pd_count(Pd_train(i));
-    Pd_prime_given_Pd = Pd_prime_given_Pd + ...
-        model.pd_prime_given_pd_count([Pd_prime_train(i) Pd_train(i)]);
-    Xb_given_Pd = Xb_given_Pd + ...
-        model.xb_given_pd_count([Xb_train(i) Pd_train(i)]);
-    Xh_given_Pd = Xh_given_Pd + ...
-        model.xh_given_pd_count([Xh_train(i) Pd_train(i)]);
-    Xt_given_Pd = Xt_given_Pd + ...
-        model.xt_given_pd_count([Xt_train(i) Pd_train(i)]);
+    Pd_count = Pd_count + model.pd_count(Pd_train(i));
+    Pd_prime_given_Pd_count = Pd_prime_given_Pd_count + model.pd_prime_given_pd_count([Pd_prime_train(i) Pd_train(i)]);
+    Xb_prime_given_Pd_prime_count = ...
+        Xb_prime_given_Pd_prime_count + model.xb_given_pd_count([Xb_prime_train(i) Pd_prime_train(i)]);
+    Xh_prime_given_Pd_prime_count = ...
+        Xh_prime_given_Pd_prime_count + model.xh_given_pd_count([Xh_prime_train(i) Pd_prime_train(i)]);
+    Xt_prime_given_Pd_prime_count = ...
+        Xt_prime_given_Pd_prime_count + model.xt_given_pd_count([Xt_prime_train(i) Pd_prime_train(i)]);
 end
--- +BN1/M_step.m
+++ +BN1/M_step.m
@@ -3,8 +3,8 @@
 % parameter k of laplacian smoothing
 k = get_laplace_k;
 
-P_Pd = (Pd + k) / (row + k*2);
-P_Pd_prime_given_Pd = (Pd_prime_given_Pd + k) / (row + k*2);
-P_Xb_prime_given_Pd_prime = (Xb_given_Pd + k) / (row + k*3);
-P_Xh_prime_given_Pd_prime = (Xh_given_Pd + k) / (row + k*3);
-P_Xt_prime_given_Pd_prime = (Xt_given_Pd + k) / (row + k*3);
+P_Pd = (Pd_count + k) / (row + k*2);
+P_Pd_prime_given_Pd = (Pd_prime_given_Pd_count + k) / (row + k*2);
+P_Xb_prime_given_Pd_prime = (Xb_prime_given_Pd_prime_count + k) / (row + k*3);
+P_Xh_prime_given_Pd_prime = (Xh_prime_given_Pd_prime_count + k) / (row + k*3);
+P_Xt_prime_given_Pd_prime = (Xt_prime_given_Pd_prime_count + k) / (row + k*3);
